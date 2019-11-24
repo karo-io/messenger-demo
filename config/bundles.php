f2b4b05a4be78e507b1716627e78185765e9949f
Peter Dietrich <xosofox@gmail.com>
2019-11-24 23:37:56 +0100

    BUNDLE move it into karo namespace

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,8 @@
 <?php
 
-use KaroIO\MessengerMonitor\KaroIOMessengerMonitorBundle;
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    KaroIOMessengerMonitorBundle::class => ['all' => true],
+    Karo\MessengerMonitor\KaroMessengerMonitorBundle::class => ['all' => true],
 ];
