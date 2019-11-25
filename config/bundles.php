6071c315007e6c2f00c6ebc0c49a2fb0a3b5baf6
Peter Dietrich <xosofox@gmail.com>
2019-11-25 02:18:58 +0100

    BUNDLE KaroIO namespace

--- config/bundles.php
+++ config/bundles.php
@@ -4,5 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Karo\MessengerMonitor\KaroMessengerMonitorBundle::class => ['all' => true],
+    KaroIO\MessengerMonitor\KaroIOMessengerMonitorBundle::class => ['all' => true],
 ];
